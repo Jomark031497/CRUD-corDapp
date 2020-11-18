2ebeca8d4945479966495dd2be17f67b507227f0
Jomark pangan <jomarkrazonpangan@gmail.com>
2020-11-18 10:10:25 +0800

    Added state

--- contracts/src/main/kotlin/com/template/states/TemplateState.kt
+++ contracts/src/main/kotlin/com/template/states/TemplateState.kt
@@ -3,10 +3,18 @@ package com.template.states
 import com.template.contracts.TemplateContract
 import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.ContractState
-import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+
 
 // *********
 // * State *
 // *********
 @BelongsToContract(TemplateContract::class)
-data class TemplateState(val data: String, override val participants: List<AbstractParty> = listOf()) : ContractState
+class IOUState(val name: Party,
+               val iouValue: Int,
+               val age: Int,
+               val address: String,
+               val status: StatusEnums ) : ContractState {
+    override val participants get() = listOf(name);
+
+}
