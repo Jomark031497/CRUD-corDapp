d32488dc7a2b148805d2ddd3d46f40c18372b75d
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-11-25 15:56:38 +0800

    Added comments

--- contracts/src/main/kotlin/com/template/states/UserState.kt
+++ contracts/src/main/kotlin/com/template/states/UserState.kt
@@ -7,8 +7,11 @@ import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.Party
 
-/*
-The State
+/**
+ * **********
+    The State
+ * **********
+
 * Represents on ledger facts
 * States are evolved by marking the current state as historic and creating an updated state
 * Each node has a vault where it stores any relevant states to itself
@@ -16,15 +19,14 @@ The State
 * can contain any kind of information
  */
 
-
-/*
-ContractState       requires a property that returns the participants of the state.
-linearId            UniqueIdentifier // a UUID. ID is generated from the UUID class.
-LinearState         evolves by superseding itself. As such, when the state is updated it
+/**
+ * ContractState       requires a property that returns the participants of the state.
+ * linearId            UniqueIdentifier // a UUID. ID is generated from the UUID class.
+ * LinearState         evolves by superseding itself. As such, when the state is updated it
                     should be included as an input of a transaction with a newer version being output.
-Participants        The parties defined within this list determine who gets to have the state saved to
+ * Participants        The parties defined within this list determine who gets to have the state saved to
                     their own vault/database.
-@BelongsToContract  Annotation which binds a state to a contract.
+ * @BelongsToContract  Annotation which binds a state to a contract.
 */
 @BelongsToContract(UserContract::class)
 class UserState(val name: String,
@@ -36,3 +38,6 @@ class UserState(val name: String,
                 override val linearId: UniqueIdentifier,
                 override val participants : List<Party>
 ) : LinearState
+ /**
+        linearState means the state may evolve overtime
+ */
