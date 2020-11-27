2e14b3f94d58cc68a030d355432b46097c1fb29f
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-11-27 14:06:53 +0800

    Used function overloading

--- workflows/src/main/kotlin/com/template/flows/BaseFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/BaseFlow.kt
@@ -4,15 +4,24 @@ import co.paralleluniverse.fibers.Suspendable
 import com.template.contracts.UserContract
 import com.template.states.UserState
 import net.corda.core.contracts.Command
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.flows.CollectSignaturesFlow
 import net.corda.core.flows.FinalityFlow
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
 import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 
 abstract class BaseFlow : FlowLogic<SignedTransaction>(){
+
+    fun getVaultData(linearId: UniqueIdentifier) : StateAndRef<UserState> {
+        val queryCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(linearId))
+        return serviceHub.vaultService.queryBy<UserState>(queryCriteria).states.single()
+    }
 
     fun transaction(state : UserState): TransactionBuilder {
         val notary: Party = serviceHub.networkMapCache.notaryIdentities.first()
@@ -21,6 +30,20 @@ abstract class BaseFlow : FlowLogic<SignedTransaction>(){
 
         builder.addOutputState(state, UserContract.ID)
         builder.addCommand(issueCommand)
+        return builder
+    }
+
+    fun transaction(state: UserState,
+                            dataState: StateAndRef<UserState>): TransactionBuilder {
+
+        val notary: Party = serviceHub.networkMapCache.notaryIdentities.first()
+        val updateCommand = Command(UserContract.Commands.Update(), state.participants.map { it.owningKey })
+        val builder = TransactionBuilder(notary = notary)
+
+        builder
+                .addInputState(dataState)
+                .addOutputState(state, UserContract.ID)
+                .addCommand(updateCommand)
         return builder
     }
 
